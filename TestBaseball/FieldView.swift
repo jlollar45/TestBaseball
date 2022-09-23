d2738fa6688f955cc9e5a23025cb208d5d4f2e54
John Lollar <jlollar45@gmail.com>
2022-09-22 23:15:56 -0500

    Pitch Dots appear as different colors based on whether it is a ball or a strike

--- TestBaseball/FieldView.swift
+++ TestBaseball/FieldView.swift
@@ -11,10 +11,6 @@ struct FieldView: View {
     
     @State private var pitches: [Pitch] = []
     
-    enum PitchResult {
-        case ball, strike
-    }
-    
     let strikeZone = (1...9)
     let outerEdge = (10...13)
     
@@ -32,6 +28,15 @@ struct FieldView: View {
     var body: some View {
         GeometryReader { geo in
             ZStack {
+                Rectangle()
+                    .fill(.white)
+                    .border(.black)
+                    .frame(width: geo.size.width * 0.99, height: geo.size.height * 0.8)
+                    .position(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).midY)
+                    .onTapGesture(coordinateSpace: .global) { location in
+                        pitches.append(Pitch(location: location, result: .ball))
+                    }
+                
                 LazyVGrid(columns: outerEdgeColumns, spacing: 0) {
                     ForEach(outerEdge, id:\.self) {outerZone in
                         Rectangle()
@@ -39,13 +44,13 @@ struct FieldView: View {
                             .border(.black)
                             .frame(height: geo.size.height * 0.31)
                             .onTapGesture(coordinateSpace: .global) { location in
-                                pitches.append(Pitch(location: location))
+                                pitches.append(Pitch(location: location, result: .ball))
                                 print(outerZone)
                             }
                     }
                 }
                 .frame(width: geo.size.width * 0.85)
-                .position(x: geo.size.width / 2, y: geo.size.height / 2)
+                .position(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).midY)
                 
                 LazyVGrid(columns: strikeZoneColumns, spacing: 0) {
                     ForEach(strikeZone, id:\.self) {partOfZone in
@@ -54,41 +59,23 @@ struct FieldView: View {
                             .border(.black)
                             .frame(height: geo.size.height * 0.165)
                             .onTapGesture(coordinateSpace: .global) { location in
-                                pitches.append(Pitch(location: location))
+                                pitches.append(Pitch(location: location, result: .strike))
                                 print(partOfZone)
                             }
                     }
                 }
                 .frame(width: geo.size.width * 0.65)
-                .position(x: geo.size.width / 2, y: geo.size.height / 2)
+                .position(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).midY)
                 
                 ForEach(pitches, id: \.id) { pitch in
                     Circle()
-                        .fill(.red)
+                        .fill(pitch.result == .strike ? .red : .green)
                         .frame(width: geo.size.width * 0.05, height: geo.size.width * 0.05)
                         .position(x: pitch.location.x, y: pitch.location.y)
                 }
             }
             .ignoresSafeArea()
         }
-        
-       
-//        ZStack {
-//            Image("strikeZone")
-//                .resizable()
-//                .scaledToFit()
-//                .onTapGesture(coordinateSpace: .global) { location in
-//                    pitchLocations.append(location)                    print(location)
-//                }
-//
-//            ForEach(pitchLocations, id: \.x) {location in
-//                Circle()
-//                    .fill(.red)
-//                    .frame(width: 20, height: 20)
-//                    .position(x: location.x, y: location.y)
-//            }
-//        }
-//        .ignoresSafeArea()
     }
 }
 
@@ -102,4 +89,9 @@ struct FieldView_Previews: PreviewProvider {
 struct Pitch {
     let id = UUID()
     let location: CGPoint
+    let result: PitchResult
+}
+
+enum PitchResult {
+    case ball, strike
 }
