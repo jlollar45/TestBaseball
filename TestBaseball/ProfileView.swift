5eeaaace4a72efafa27b77334141158cdf3d5260
John Lollar <jlollar45@gmail.com>
2022-10-04 23:39:06 -0500

    Made rest of App Tab Views use NavStack

--- TestBaseball/ProfileView.swift
+++ TestBaseball/ProfileView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @ObservedObject var coordinator = Coordinator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(path: $coordinator.path) {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Profile")
+        }
     }
 }
 
